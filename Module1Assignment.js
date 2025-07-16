723696d85b23680fad1d50ec3d47fc3806da13f2
JayyMiller2023 <147433492+JayyMiller2023@users.noreply.github.com>
2025-07-16 13:41:04 -0600

    Add files via upload

---
+++ Module1Assignment.js
@@ -0,0 +1,8 @@
+
+// h1 text content using javascript
+document.getElementById("Name").textContent = "Justin (Jayy) C. Miller";
+
+// All div text content using javascript to print in the divs
+document.getElementById('Major').textContent = "Major: Bachelor's Degree in Game Design/Programmer";
+document.getElementById('Email').textContent = "Email: JMiller86022@uat.edu";
+document.getElementById('Graduation_date').textContent = "Expected Graduation Date: Somewhere around May of 2027";
